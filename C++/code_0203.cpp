1acd63f7ace27b9bbcfb4aa2e84d56cc18103ea1
MizutoKadowaki0312 <kadowaki0312.res@gmail.com>
2021-08-09 05:07:52 +0900

    update

--- C++/code_0203.cpp
+++ C++/code_0203.cpp
@@ -2,19 +2,19 @@
 #include <time.h>
 using namespace std;
 
-int main(){
+int main()
+{
     int N;
     cout << "N = ";
     cin >> N;
 
     int count = 0;
 
-    for (int i = 2; i <= N; i+=2)
+    for (int i = 2; i <= N; i += 2)
     {
         cout << "i = " << i << endl;
         count += 1;
     }
 
     cout << "反復回数 : " << count << endl;
-    
 }
