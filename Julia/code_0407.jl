12f761b8b53ae77f6b4445831f37941d8e2249d6
Kadowaki0312.res@gmail.com <kadowaki0312.res@gmail.com>
2021-08-15 16:42:03 +0900

    update

--- Julia/code_0407.jl
+++ Julia/code_0407.jl
@@ -1,24 +1,42 @@
+using BenchmarkTools
+
+
 N = 20
 
-F = Float64[]
+F = Vector{Float64}(undef, N)
 
-for i in 1:N
+for i in 1:length(F)
     if i == 1
-        push!(F, 0)
+        F[i] = 0
     elseif i == 2
-        push!(F, 1)
+        F[i] = 1
     else
-        """
-        F = Float64[] で配列のサイズを指定していないので，
-        F[i] = F[i - 1] + F[i - 2] として 
-        push!(F,F[i]) ← このときFはまだ(i-1)要素しかないので，i番目の要素を追加！と言われてもエラーになる．
-        なので，結果を別の変数に入れてあげることにする．
-        """
-        result = F[i - 1] + F[i - 2]
-        push!(F, result)
+        F[i] = F[i - 1] + F[i - 2]
     end
 end
 
-for i in 1:N
-    @show F[i]
+for i in 1:length(F)
+    println(F[i])
 end
+
+
+
+"""
+関数化してしまえ
+"""
+
+function fibo(N)
+    F = Vector{Float64}(undef, N)
+    for i in 1:length(F)
+        if i == 1
+            F[i] = 0
+        elseif i == 2
+            F[i] = 1
+        else
+            F[i] = F[i - 1] + F[i - 2]
+        end
+    end
+end
+
+
+fibo(20)
