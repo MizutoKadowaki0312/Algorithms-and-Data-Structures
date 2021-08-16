3761ed7d433b5beeed44d889c3ac2bbb059a533f
Kadowaki0312.res@gmail.com <kadowaki0312.res@gmail.com>
2021-08-17 00:43:04 +0900

    update

--- C++/code_0409.cpp
+++ C++/code_0409.cpp
@@ -5,9 +5,8 @@
 using namespace std;
 
 /*
-    When you pass vector<int> then function gets a copy of that vector. You can do anything you want with it in the function and your original data would not change.
-    
-    When you pass vector<int>& then function gets the reference which means that any changes in the function would modify the original data.
+    参照渡し(https://wa3.i-3-i.info/word16070.html)
+    を const vector<int> &a で与える．
 */
 
 bool func(int i, int w, const vector<int> &a)
@@ -23,5 +22,48 @@ bool func(int i, int w, const vector<int> &a)
         {
             return false;
         }
+    }
+
+    //a[i-1]を選ばない場合
+    if (func(i - 1, w, a))
+    {
+        return true;
+    }
+
+    //a[i-1]を選ぶ場合
+    if (func(i - 1, w - a[i - 1], a))
+    {
+        return true;
+    }
+
+    return false;
+}
+
+int main()
+{
+    //入力
+    int N;
+    int W;
+
+    cout << "N = ";
+    cin >> N;
+
+    cout << "W = ";
+    cin >> W;
+
+    vector<int> a(N);
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> a[i];
+    }
+
+    if (func(N, W, a))
+    {
+        cout << "Yes" << endl;
+    }
+    else
+    {
+        cout << "No" << endl;
     }
 }
