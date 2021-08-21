ba65d45ff72541bc960c50aa83fc02a063c75c45
Kadowaki0312.res@gmail.com <kadowaki0312.res@gmail.com>
2021-08-22 03:48:54 +0900

    コメントを追加

--- C++/code_0503.cpp
+++ C++/code_0503.cpp
@@ -5,6 +5,7 @@
 using namespace std;
 
 const long long INF = 1LL << 60; //2^60 を表す．十分大きい値を設定する．long は int よりも大きい桁の整数を扱うときに使う．long long は longよりも大きい桁の整数を使うときに使う．
+
 template <class T>
 void chmin(T &a, T b)
 {
@@ -35,7 +36,10 @@ int main()
     //ループ
     for (int i = 0; i < N; i++)
     {
+        //一旦ここで処理がある
         chmin(dp[i], dp[i - 1] + abs(h[i] - h[i - 1]));
+        // i > 1の時，以下の処理もする．
+        // 配列dpが配列外参照を起こさないようにする．
         if (i > 1)
         {
             chmin(dp[i], dp[i - 2] + abs(h[i] - h[i - 2]));
